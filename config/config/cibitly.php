5cfa7b5d8ea884e341ed40c11d710440559a2ee9
Shawn Crigger <support@s-vizion.com>
2012-03-11 16:10:45 -0400

    Fixing errors reported on getsparks

---
+++ config/config/cibitly.php
@@ -0,0 +1,10 @@
+<?php
+
+# The base URL for API calls to twitter
+$config['bitly_url'] = 'http://api.bit.ly/v3/';
+
+# Your login bit.ly login id
+$config['login'] = 'yourloginid';
+
+# Your login bit.ly api key
+$config['apikey'] = 'yourapikey';
